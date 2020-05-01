d7f34a7756222ffea89e9fd068fe8bd7ed9d4ea5
Gao Ya'nan <abutter.gao@gmail.com>
2020-05-01 20:27:59 +0800

    Add source code for Chapter Getting Started
    I could pass the test

---
+++ p00_Getting_Started/c00/s01_one.v
@@ -0,0 +1,15 @@
+//
+//
+// https://hdlbits.01xz.net/wiki/Step_one
+//
+//
+
+`default_nettype none
+
+module top_module (
+    output  one
+);
+
+    assign one = 1'b1;
+
+endmodule
---
+++ p00_Getting_Started/c00/s02_zero.v
@@ -0,0 +1,15 @@
+//
+//
+// https://hdlbits.01xz.net/wiki/Zero
+//
+//
+
+`default_nettype none
+
+module top_module (
+    output  zero
+);
+
+    assign zero = 1'b0;
+
+endmodule
